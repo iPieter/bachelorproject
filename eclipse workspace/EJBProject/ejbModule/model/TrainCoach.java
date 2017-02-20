406d8dc1793526c4aff97ba4da1e82656c652dc6
Pieter Delobelle <pieter.delobelle@student.kuleuven.be>
2017-02-20 12:02:55 +0100

    Deleted Main
    Deleted the Main-file used for testing

--- eclipse workspace/EJBProject/ejbModule/model/TrainCoach.java
+++ eclipse workspace/EJBProject/ejbModule/model/TrainCoach.java
@@ -14,6 +14,7 @@ public class TrainCoach implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 
 	@Lob
