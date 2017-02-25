a3205c827004a557c61cfb459dff723e34f7c1b7
Pieter Delobelle <pieter.delobelle@student.kuleuven.be>
2017-02-25 17:26:51 +0100

    Started creating test user

--- eclipse workspace/EJBProject/ejbModule/service/UserService.java
+++ eclipse workspace/EJBProject/ejbModule/service/UserService.java
@@ -4,9 +4,15 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import model.User;
+
 @Stateless
 public class UserService
 {
 	@PersistenceContext(unitName="EJBProject")
 	private EntityManager em;
+	
+	//create a test user
+	User u = new User();
+	u.
 }
