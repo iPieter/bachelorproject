f916dadf61a7ec71f179f75e459163d3705473e9
Matt <de.lange.matthias@gmail.com>
2017-02-27 14:31:13 +0100

    Working merge

--- eclipse workspace/EJBProject/ejbModule/be/kuleuven/cs/gent/projectweek/services/InternalDatafetchService.java
+++ eclipse workspace/EJBProject/ejbModule/be/kuleuven/cs/gent/projectweek/services/InternalDatafetchService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
@@ -14,14 +16,19 @@ import be.kuleuven.cs.gent.projectweek.model.*;
 @Stateless
 public class InternalDatafetchService {
 	
-	//@PersistenceContext(unitName="EJBProject")
-	//private EntityManager em;
-	
-	public List<Workplace> getAllTraincoachDepots(){
-		//TypedQuery<Workplace> query= em.createNamedQuery("findAll", Workplace.class);
-		//return query.getResultList();	
-
-		return null;
+	public List<Workplace> getAllTraincoachDepots(){	
+		
+		EntityManagerFactory emf =  Persistence.createEntityManagerFactory("EJBProject");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		
+		TypedQuery<Workplace> query= em.createNamedQuery("findAll", Workplace.class);
+		
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+		
+		return query.getResultList();	
 	}
 	
 }
