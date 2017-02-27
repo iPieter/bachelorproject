996f61863a13a81b556f7ff67152bfe242f3532b
Matt <de.lange.matthias@gmail.com>
2017-02-27 13:52:25 +0100

    front end test

--- eclipse workspace/DWPProject/src/primefaces/MenuView.java
+++ eclipse workspace/DWPProject/src/primefaces/MenuView.java
@@ -1,11 +1,54 @@
 package primefaces;
+ 
+import controllers.*;
+import model.Workplace;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.MenuModel;
  
 @ManagedBean
 public class MenuView {
+     
+    private MenuModel model;
+    
+    @Inject
+    private TraincoachDepotController controller;
+    
+    @PostConstruct
+    public void init() {
+        model = new DefaultMenuModel();
+        //controller=new TraincoachDepotController();
+        		
+        //Fetch Data
+        List<Workplace> datalist=controller.getAllTraincoachDepots();
+        
+        //First submenu
+        DefaultSubMenu firstSubmenu = new DefaultSubMenu("Traincoach Depots");
+         
+        DefaultMenuItem item;
+        for(Workplace w: datalist){
+        	item = new DefaultMenuItem(w.getName());
+	        item.setUrl("http://www.primefaces.org");
+	        item.setIcon("ui-icon-home");
+	        firstSubmenu.addElement(item);
+        }
+         
+        model.addElement(firstSubmenu);
+    }
+ 
+    public MenuModel getModel() {
+        return model;
+    }   
      
     public void save() {
         addMessage("Success", "Data saved");
