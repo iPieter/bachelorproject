1ed315259bd1cc0443c33f53149c3014ea270632
AntonDanneels <antondanneels@hotmail.com>
2017-03-06 18:17:44 +0100

    Documented ProcessedSensorDataEJB

--- eclipse workspace/EJBProject/ejbModule/bachelorproject/ejb/ProcessedSensorDataEJB.java
+++ eclipse workspace/EJBProject/ejbModule/bachelorproject/ejb/ProcessedSensorDataEJB.java
@@ -9,10 +9,25 @@ import javax.persistence.TypedQuery;
 
 import bachelorproject.model.ProcessedSensorData;
 
+/**
+ * 	Defines the Entity Java Bean for the ProcessedSensorData Entity.
+ *  <p>
+ *  This class allows for the controller to manipulate and fetch specific
+ *  ProcessedSensorData instances. It validates new objects and handles 
+ *  errors when, for example, no entries in the databse exist.
+ * */
 @Named
 @Stateless
 public class ProcessedSensorDataEJB
 {
+	/**
+	 * 	Fetches a ProcessedSensorData from the database if it exists.
+	 *  <p>
+	 *  Uses an EntityManager to fetch the specified object from the database.
+	 *  This method will return null if no object is found or if another error occures.
+	 *  @param id The ID of the ProcessedSensorData object thats needed.
+	 *  @return A ProcessedSensorData object if found, null otherwise.
+	 * */
 	public ProcessedSensorData getProcessedSensorDataByID( int id )
 	{
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory( "EJBProject" );
