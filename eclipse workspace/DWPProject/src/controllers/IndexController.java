259cd444304cb419cdd058267b0cbcc2fd537a8e
Matt <de.lange.matthias@gmail.com>
2017-03-15 09:05:09 +0100

    Index page ready without heatmap

--- eclipse workspace/DWPProject/src/controllers/IndexController.java
+++ eclipse workspace/DWPProject/src/controllers/IndexController.java
@@ -1,9 +1,7 @@
 package controllers;
 
 import java.io.Serializable;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
@@ -13,6 +11,7 @@ import javax.inject.Named;
 import bachelorproject.ejb.IssueEJB;
 import bachelorproject.model.Issue;
 import bachelorproject.model.IssueStatus;
+import bachelorproject.model.User;
 import bachelorproject.services.UserService;
 
 @Named
@@ -53,6 +52,40 @@ public class IndexController implements Serializable{
 	 */
 	public List<Issue> findOperatorInProgressIssues(){
 		return issueEJB.findOperatorIssues( userService.getUser().getId(), IssueStatus.IN_PROGRESS  );
+	}
+	
+	/**
+	 * Returns a List of Issue objects for the current User(UserRole=OPERATOR).
+	 * The list contains an overview of the Issues that the Operator assigned.
+	 * 
+	 * @author Matthias De Lange
+	 * @version 0.0.1
+	 * @return List of Issue objects
+	 * @see IssueEJB
+	 */
+	public List<Issue> findOperatorAssignedIssues(){
+		return issueEJB.findOperatorIssues( userService.getUser().getId(), IssueStatus.ASSIGNED  );
+	}
+	
+	/**
+	 * The user avatar is a non-guessable file, this function returns the name
+	 * of the file. Appending it with it's image format (".png", ".jpg") will 
+	 * result in the correct file.
+	 * <p>
+	 * The REST api will do this for you. So just call <code>/rest/assets/user/#</code>.
+	 * 
+	 * @author Pieter Delobelle
+	 * @version 0.0.1
+	 * @return String with the non-guessable location of the user avatar. 
+	 */
+	public String getAvatar()
+	{
+		User u = userService.getUser();
+		if (u != null)
+		{
+			return u.getImageHash();
+		}
+		return "";		
 	}
 	
 	// GETTERS & SETTERS	
