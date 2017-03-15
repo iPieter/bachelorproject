dc83f6d6a1d1bb3b8e8b7fcb9af7a99872180953
Matt <de.lange.matthias@gmail.com>
2017-03-15 07:42:13 +0100

    Displaying warning msg when no activity in last 30 days

--- eclipse workspace/DWPProject/WebContent/js/index.js
+++ eclipse workspace/DWPProject/WebContent/js/index.js
@@ -1,6 +1,4 @@
-
 var operator_id =null;
-var data_history = $("#operator_statistics_history").val();
 var data_donut=null;
 
 $(document).ready(function() {
@@ -21,42 +19,48 @@ function setDonutView(data)
 {
 	
 	console.log(data);
-	
-	Highcharts.chart('donut_graph', {
-	    chart: {
-	        plotBackgroundColor: null,
-	        plotBorderWidth: 0,
-	        plotShadow: false
-	    },
-	    title: {
-	        text: 'Problemen<br>Deze<br>Maand',
-	        align: 'center',
-	        verticalAlign: 'middle',
-	        y: 40
-	    },
-	    tooltip: {
-	        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-	    },
-	    plotOptions: {
-	        pie: {
-	            dataLabels: {
-	                enabled: false,
-	                distance: -50,
-	                style: {
-	                    fontWeight: 'bold',
-	                    color: 'white'
-	                }
-	            },
-	            startAngle: -95,
-	            endAngle: 95,
-	            center: ['50%', '75%']
-	        }
-	    },
-	    series: [{
-	        type: 'pie',
-	        name: 'Percentage',
-	        innerSize: '60%',
-	        data: data.issue_counts
-	    }]
-	});
+	console.log(data.issue_counts[0][1]);
+	if(data.issue_counts[0][1]==0 && data.issue_counts[1][1]==0 && data.issue_counts[2][1]==0){
+		var display_msg="<b>Welcome back!</b> Laatste 30 dagen nog geen activiteit geregistreerd, let's get started.";
+		$("#display_when_empty_donut").html(display_msg);
+	}
+	else{
+		Highcharts.chart('donut_graph', {
+		    chart: {
+		        plotBackgroundColor: null,
+		        plotBorderWidth: 0,
+		        plotShadow: false
+		    },
+		    title: {
+		        text: 'Activiteit<br>Afgelopen<br>30 Dagen',
+		        align: 'center',
+		        verticalAlign: 'middle',
+		        y: 40
+		    },
+		    tooltip: {
+		        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+		    },
+		    plotOptions: {
+		        pie: {
+		            dataLabels: {
+		                enabled: false,
+		                distance: -50,
+		                style: {
+		                    fontWeight: 'bold',
+		                    color: 'white'
+		                }
+		            },
+		            startAngle: -95,
+		            endAngle: 95,
+		            center: ['50%', '75%']
+		        }
+		    },
+		    series: [{
+		        type: 'pie',
+		        name: 'Percentage',
+		        innerSize: '60%',
+		        data: data.issue_counts
+		    }]
+		});
+	}
 }
