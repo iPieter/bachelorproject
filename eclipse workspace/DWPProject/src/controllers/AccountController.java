4fa3b44c8f8bd589f73007e24725074e3c63ebcb
Pieter Delobelle <pieter.delobelle@student.kuleuven.be>
2017-03-26 17:48:55 +0200

    Fixed #77

--- eclipse workspace/DWPProject/src/controllers/AccountController.java
+++ eclipse workspace/DWPProject/src/controllers/AccountController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -13,9 +14,11 @@ import bachelorproject.services.UserService;
 
 /**
  * Controller for the xhtml page where a user can manage his/her account.
- * 
+ * <p>
  * At the moment, there's only one function: changing the user image via a 
  * REST-api. So for this, we need a hidden input with the email.
+ * <p>
+ * When starting up, 
  * 
  * @author Pieter Delobelle
  * @version 0.1.0
@@ -29,6 +32,24 @@ public class AccountController implements Serializable
 	
 	@Inject
 	private UserService userService;
+	@Inject
+	private AuthenticationController authC;
+	
+	@EJB
+	private UserEJB userEJB;
+	
+	/**
+	 * Dirty hack to make sure the updates on the database by the REST api are
+	 * represented on the client's view.
+	 * 
+	 * @author Pieter Delobelle
+	 * @version 0.1.0
+	 */
+	@PostConstruct
+	public void updateUser()
+	{
+		userService.setUser(userEJB.findUserById(userService.getUser().getId()));
+	}
 	
 	/**
 	 * @author Pieter Delobelle
@@ -39,6 +60,16 @@ public class AccountController implements Serializable
 	public String getUserEmail()
 	{
 		return userService.getUser().getEmail();
+	}
+	
+	/**
+	 * @author Pieter Delobelle
+	 * @version 1.0.0
+	 * @return Image hash of the user
+	 */
+	public String getAvatar()
+	{
+		return userService.getUser().getImageHash();
 	}
 
 	
