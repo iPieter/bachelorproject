245031f401b96683a8651e8f67ee12ba5238fa53
Pieter Delobelle <pieter.delobelle@gmail.com>
2017-04-05 15:20:14 +0200

    fixed server.js ip

--- realtime_server/server.js
+++ realtime_server/server.js
@@ -28,13 +28,13 @@ request( 'http://localhost:8080/DWPProject-0.0.1-SNAPSHOT/rest/live_data/registe
 
         console.log( content );
 
-        request.post( 'http://10.108.16.203:8080/DWPProject-0.0.1-SNAPSHOT/rest/live_data/add_entry/' + content, function( error, response, body )
+        request.post( 'http://localhost:8080/DWPProject-0.0.1-SNAPSHOT/rest/live_data/add_entry/' + content, function( error, response, body )
         {
         });
 
         if( index >= 2700 || index > data.yaw.length )
         {
-            request( 'http://10.108.16.203:8080/DWPProject-0.0.1-SNAPSHOT/rest/live_data/stop/' + lsdID, function( error, response, body )
+            request( 'http://localhost:8080/DWPProject-0.0.1-SNAPSHOT/rest/live_data/stop/' + lsdID, function( error, response, body )
             {
                 clearInterval( timer );
             } );
