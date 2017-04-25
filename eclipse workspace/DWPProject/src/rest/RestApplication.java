a5ea3dd0dc7bac5647886fc73f098b6b2f12ca7b
AntonDanneels <antondanneels@hotmail.com>
2017-04-25 11:03:26 +0200

    Documented RestApplication

--- eclipse workspace/DWPProject/src/rest/RestApplication.java
+++ eclipse workspace/DWPProject/src/rest/RestApplication.java
@@ -3,6 +3,9 @@ package rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+/**
+ * 	Class required to allow the project to have REST endpoints
+ * */
 @ApplicationPath( "/rest" )
 public class RestApplication extends Application
 {
